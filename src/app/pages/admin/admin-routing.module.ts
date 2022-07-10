8375c3dfd2ce3fd856cf57f0986ae9574eee7297
RSBHARATHRS <78311020+RSBHARATHRS@users.noreply.github.com>
2022-07-10 21:02:20 +0530

    Some design issues has been sorted out

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
