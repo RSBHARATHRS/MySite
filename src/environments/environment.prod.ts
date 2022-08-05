4254c527b9886d0eb67b87aefcb1adfe0fa03c50
Bharath <bharath.r@idp.com>
2022-08-05 14:52:55 +0530

    expanding card has been added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAa4jyvRv2X9opJKXElFM16GVTCXecQmXw",
+    authDomain: "my-website-c0172.firebaseapp.com",
+    projectId: "my-website-c0172",
+    storageBucket: "my-website-c0172.appspot.com",
+    messagingSenderId: "798825414312",
+    appId: "1:798825414312:web:72fe6a059f5592e02c4c9d",
+    measurementId: "G-YQH5CD39T1"
+  }
 };
